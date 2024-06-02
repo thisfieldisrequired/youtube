1d9974c58400193c4d932cfcf9f5e522f7a5fd44
sine <sudodebug@gmail.com>
2024-06-02 13:49:04 +0500

    Crated and incluted api router

--- youtube-application/main.py
+++ youtube-application/main.py
@@ -1,7 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api import router as api_router
+
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix="/api",
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
